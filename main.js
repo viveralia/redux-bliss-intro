78496f398a17fc1800f9c3b3da0f1ac28a8a6bfc
viveralia <edgarv10@gmail.com>
2019-12-05 18:05:30 -0600

    Redux app done

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // import * as Redux from "redux";
+import { createStore } from "redux";
 
 // Nodes
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
-const todos = {
+let todos = {
   0: {
     text: "Ir al cine",
     done: false
@@ -21,6 +22,8 @@ const todos = {
 // Functions
 const drawTodos = () => {
   ul.innerHTML = "";
+  // Sustituimos los todos antes de dibujar
+  todos = store.getState();
   for (const key in todos) {
     let li = document.createElement("li");
     const doneClass = todos[key].done ? "done" : "";
@@ -35,29 +38,87 @@ const drawTodos = () => {
 
 const setListeners = li => {
   li.addEventListener("click", e => {
-    console.log(e.target);
     if (e.target.getAttribute("data-action") === "delete") {
       const key = e.target.getAttribute("data-id");
-      delete todos[key];
-      drawTodos();
+      // Redux: Borrando
+      store.dispatch({
+        type: "DELETE_TODO",
+        id: key
+      });
+      // delete todos[key];
+      // drawTodos();
       return;
     }
     const key = e.target.id;
     todos[key].done = !todos[key].done;
-    drawTodos();
+    store.dispatch({
+      type: "UPDATE_TODO",
+      todo: todos[key]
+    });
+
+    // Redux: Al estar suscrito, Redux llama a la función drawTodos cuando detecta un cambio
+    // drawTodos();
   });
 };
 
 // Listeners
 input.addEventListener("keydown", e => {
   if (e.key === "Enter") {
+    // const text = e.target.value;
+    // const id = Object.keys(todos).length;
+    // todos[id] = { text, done: false };
+    // drawTodos();
+    // input.value = "";
+
+    // Con Redux
     const text = e.target.value;
-    const id = Object.keys(todos).length;
-    todos[id] = { text, done: false };
-    drawTodos();
+    const todo = { text, done: false };
+    // Redux: EL método dispatch ejecuta acciones del reducer y recibe como parámetro un objeto con lo que hayamos programado (type y todo)
+    store.dispatch({
+      type: "CREATE_TODO",
+      todo
+    });
+    // Limpiamos el campo
     input.value = "";
   }
 });
+
+/*******************************/
+/************ REDUX ************/
+/*******************************/
+
+// Reducer: una función que se dedica a trabajar con los datos que viven en el store
+const todosReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "CREATE_TODO":
+      // Redux necesita inmutabilidad, por lo que no es recomendable hacer push o algo del estilo:
+      // { state[Object.keys(state).length] : action.todo }
+      // Es mejor reemplazar el objeto anterior haciendo una copia con el spread operator
+      // NOTA: Asignamos la propiedad id a cada todo para después poder actualizarla con el update_todo
+      action.todo["id"] = Object.keys(state).length;
+      return { ...state, [Object.keys(state).length]: action.todo };
+    case "UPDATE_TODO":
+      return { ...state, [action.todo.id]: action.todo };
+    case "DELETE_TODO":
+      delete state[action.id];
+      return { ...state };
+    default:
+      return state;
+  }
+};
+
+// Store: almacén de datos - Se crea con el método createStore de redux y el reducer se pasa como parámetro
+let store = createStore(todosReducer, {
+  // Como segundo parámetro, podemos decirle a redux con qué data queremos inicializar el store
+  0: {
+    id: 0,
+    text: "Crear store",
+    done: true
+  }
+});
+
+// Subscribe: Cuando escuche algún cambio en el store, entonces dibujaré los todos de nuevo
+store.subscribe(drawTodos);
 
 // Init
 drawTodos();
