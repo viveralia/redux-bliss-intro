c750b2cd076f43d296b5be57a1a0ce1ee030fe8b
viveralia <edgarv10@gmail.com>
2019-12-09 10:57:14 -0600

    Done

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // import * as Redux from "redux";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 // Nodes
-const input = document.querySelector("input");
+const input = document.querySelector("#todo-input");
+const addEmail = document.querySelector("#addEmail");
 const ul = document.querySelector("ul");
+const emailsList = document.querySelector("#emails");
 let todos = {
   0: {
     text: "Ir al cine",
@@ -23,7 +25,7 @@ let todos = {
 const drawTodos = () => {
   ul.innerHTML = "";
   // Sustituimos los todos antes de dibujar
-  todos = store.getState();
+  todos = store.getState().todos;
   for (const key in todos) {
     let li = document.createElement("li");
     const doneClass = todos[key].done ? "done" : "";
@@ -34,6 +36,30 @@ const drawTodos = () => {
     setListeners(li);
     ul.appendChild(li);
   }
+};
+
+const drawEmails = () => {
+  emailsList.innerHTML = "";
+  const emails = store.getState().emails;
+  emails.forEach(email => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>${email}</span>
+      <span id="${email}">X</span>
+    `;
+    setEmailClickListener(li);
+    emailsList.appendChild(li);
+  });
+};
+
+const setEmailClickListener = li => {
+  li.addEventListener("click", e => {
+    const email = e.target.id;
+    store.dispatch({
+      type: "DELETE_EMAIL",
+      email
+    });
+  });
 };
 
 const setListeners = li => {
@@ -83,6 +109,16 @@ input.addEventListener("keydown", e => {
   }
 });
 
+addEmail.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    store.dispatch({
+      type: "ADD_EMAIL",
+      email: e.target.value
+    });
+    e.target.value = "";
+  }
+});
+
 /*******************************/
 /************ REDUX ************/
 /*******************************/
@@ -106,19 +142,44 @@ const todosReducer = (state = {}, action) => {
       return state;
   }
 };
+// Segundo reducer para correos
+const emailsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_EMAIL":
+      return [...state, action.email];
+    // To "delete", we can filter all the other elements that are differente to the email passed as argument
+    case "DELETE_EMAIL":
+      return [...state.filter(email => email !== action.email)];
+    default:
+      return state;
+  }
+};
+
+// combineReducers: Debemos pasar ambos reducers dentro del store de redux, pero antes, debemos combinarlos. Para esi usamos el método combineReducers de redux
+let rootReducer = combineReducers({
+  todos: todosReducer,
+  emails: emailsReducer
+});
 
 // Store: almacén de datos - Se crea con el método createStore de redux y el reducer se pasa como parámetro
-let store = createStore(todosReducer, {
+let store = createStore(rootReducer, {
   // Como segundo parámetro, podemos decirle a redux con qué data queremos inicializar el store
-  0: {
-    id: 0,
-    text: "Crear store",
-    done: true
-  }
+  todos: {
+    0: {
+      id: 0,
+      text: "Crear store",
+      done: true
+    }
+  },
+  emails: ["bliss@gmail.com"]
 });
 
 // Subscribe: Cuando escuche algún cambio en el store, entonces dibujaré los todos de nuevo
-store.subscribe(drawTodos);
+store.subscribe(() => {
+  drawTodos();
+  drawEmails();
+});
 
 // Init
 drawTodos();
+drawEmails();
